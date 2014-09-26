efe328ac4d56f9bcec9781b7e61c8acc26ff652e
Dan Branton <daniel.branton@uptimesoftware.com>
2014-09-26 14:36:49 -0400

    Update monitor.php
    updated the agent command to use the real agent side script. Also moved the agent_password into the service monitor config, instead of being hardcoded

--- pkg/files/plugins/scripts/monitor-aix-vios/monitor.php
+++ pkg/files/plugins/scripts/monitor-aix-vios/monitor.php
@@ -5,8 +5,8 @@ require('rcs_function.php');
 
 $agent_hostname = getenv('UPTIME_HOSTNAME');
 $agent_port     = getenv('UPTIME_PORT');
-$agent_password = 'vios-aix';
-$agent_cmd = '/opt/uptime-agent/scripts/vios-stubb.ksh';
+$agent_password = getenv('UPTIME_PASSWORD');
+$agent_cmd = '/opt/uptime-agent/scripts/uptime_nmon_ioadapt.ksh';
 
 $agent_output = uptime_remote_custom_monitor($agent_hostname, $agent_port, $agent_password, $agent_cmd, "");
 print $agent_output;
